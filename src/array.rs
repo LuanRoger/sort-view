6e0e00e0037de957d099807cf18ec8a97c1bf5b7
ROG <luanroger.dev@gmail.com>
2026-07-31 19:27:07 -0300

    perf: using RefCell to share data array reference

--- src/array.rs
+++ src/array.rs
@@ -1,43 +1,100 @@
+use std::{cell::RefCell, rc::Rc};
+
 use rand::RngExt;
 
-pub enum SortAlgorithm {
-    BubbleSort,
+pub trait SortAlgorithm {
+    fn new(data: Rc<RefCell<Vec<Data>>>) -> Self;
+    fn sort(&mut self);
+    fn is_sorted(&self) -> bool;
+    fn reset(&mut self, new_data_set: Rc<RefCell<Vec<Data>>>);
 }
 
-#[derive(Debug, Default)]
+pub struct BubbleSort {
+    data: Rc<RefCell<Vec<Data>>>,
+    i: usize,
+    j: usize,
+}
+
+impl SortAlgorithm for BubbleSort {
+    fn new(data: Rc<RefCell<Vec<Data>>>) -> Self {
+        Self { data, i: 0, j: 0 }
+    }
+
+    fn sort(&mut self) {
+        let data_len = self.data.borrow().len();
+        for i in 0..data_len {
+            for j in 0..data_len - i - 1 {
+                let j_value = self.data.borrow()[j].value;
+                let j_plus_1_value = self.data.borrow()[j + 1].value;
+
+                if j_value > j_plus_1_value {
+                    self.data.borrow_mut().swap(j, j + 1);
+                }
+            }
+        }
+    }
+
+    fn is_sorted(&self) -> bool {
+        self.data.borrow().iter().copied().is_sorted()
+    }
+
+    fn reset(&mut self, new_data_set: Rc<RefCell<Vec<Data>>>) {
+        self.i = 0;
+        self.j = 0;
+        self.data = new_data_set;
+    }
+}
+
+#[derive(Debug, Clone, Copy, Default)]
 pub struct Data {
     pub id: usize,
     pub value: u8,
 }
 
-pub struct SortableArray {
-    pub data: Vec<Data>,
-    sorted: bool,
-    pub sort_algorithm: SortAlgorithm,
+impl PartialEq for Data {
+    fn eq(&self, other: &Self) -> bool {
+        self.value == other.value
+    }
+}
+
+impl PartialOrd for Data {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.value.partial_cmp(&other.value)
+    }
+}
+
+pub struct SortableArray<T>
+where
+    T: SortAlgorithm,
+{
+    pub data: Rc<RefCell<Vec<Data>>>,
+    pub sort_algorithm: T,
+    pub length: usize,
     pub min: u8,
     pub max: u8,
 }
 
-impl SortableArray {
-    pub fn new(data: Vec<Data>, sort_algorithm: SortAlgorithm) -> Self {
-        let min = data.iter().map(|d| d.value).min().unwrap_or(0);
-        let max = data.iter().map(|d| d.value).max().unwrap_or(0);
+impl<T> SortableArray<T>
+where
+    T: SortAlgorithm,
+{
+    pub fn new(data: Rc<RefCell<Vec<Data>>>) -> Self {
+        let min = data.borrow().iter().map(|d| d.value).min().unwrap_or(0);
+        let max = data.borrow().iter().map(|d| d.value).max().unwrap_or(0);
+        let length = data.borrow().len();
+
+        let sort_algorithm = T::new(data.clone());
 
         Self {
-            data,
-            sorted: false,
+            data: data.clone(),
             sort_algorithm,
+            length,
             min,
             max,
         }
     }
 
-    pub fn generate_random_data(
-        size: usize,
-        sort_algorithm: SortAlgorithm,
-        max: u8,
-        min: u8,
-    ) -> Self {
+    pub fn generate_random_data(size: usize, max: u8, min: u8) -> Self {
         let mut data = Vec::new();
         let mut rng = rand::rng();
 
@@ -46,45 +103,28 @@ impl SortableArray {
             data.push(Data { id: i, value });
         }
 
-        Self::new(data, sort_algorithm)
-    }
-
-    pub fn length(&self) -> usize {
-        self.data.len()
+        Self::new(Rc::new(RefCell::new(data)))
     }
 
     pub fn shuffle_new_set(&mut self, size: Option<usize>) {
         let mut data = Vec::new();
         let mut rng = rand::rng();
-        let size = size.unwrap_or(self.data.len());
+        let size = size.unwrap_or(self.length);
 
         for i in 0..size {
             let value: u8 = rng.random_range::<u8, _>(self.min..self.max);
             data.push(Data { id: i, value });
         }
 
-        self.data = data;
-        self.sorted = false;
+        self.data = Rc::new(RefCell::new(data));
+        self.sort_algorithm.reset(self.data.clone());
     }
 
     pub fn sort(&mut self) {
-        match self.sort_algorithm {
-            SortAlgorithm::BubbleSort => self.bubble_sort(),
-        }
+        self.sort_algorithm.sort();
     }
 
     pub fn is_sorted(&self) -> bool {
-        self.sorted
-    }
-
-    fn bubble_sort(&mut self) {
-        for i in 0..self.data.len() {
-            for j in 0..self.data.len() - i - 1 {
-                if self.data[j].value > self.data[j + 1].value {
-                    self.data.swap(j, j + 1);
-                }
-            }
-        }
-        self.sorted = true;
+        self.sort_algorithm.is_sorted()
     }
 }
